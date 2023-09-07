7f52a67d6f678fabce6d94f8ebac534a634f2abe
Bektur Razzakov <brzzkv@gmail.com>
2023-09-07 18:47:39 +0600

    Authentication is fixed

--- Models/User.ts
+++ Models/User.ts
@@ -29,6 +29,10 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
 });
 
 UserSchema.pre('save',async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
   const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
   this.password = await bcrypt.hash(this.password, salt);
 
