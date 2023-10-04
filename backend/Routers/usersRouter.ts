8547d3975359f22cc2b95d6e1d3f7d32b985a2cb
Bektur Razzakov <brzzkv@gmail.com>
2023-10-04 21:48:16 +0600

    #91 A router for authenticating with Google is created on backend

--- backend/Routers/usersRouter.ts
+++ backend/Routers/usersRouter.ts
@@ -2,8 +2,12 @@ import express from "express";
 import User from "../models/User";
 import mongoose from "mongoose";
 import auth from "../middleware/auth";
+import { OAuth2Client } from "google-auth-library";
+import config from "../config";
+import { randomUUID } from "crypto";
 
 const usersRouter = express();
+const client = new OAuth2Client(config.google.clientId);
 
 usersRouter.post("/", async (req, res, next) => {
   try {
@@ -44,6 +48,53 @@ usersRouter.post("/sessions", async (req, res, next) => {
 
     res.send({
       message: "Username and password correct!",
+      user,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+usersRouter.post("/google", async (req, res, next) => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: req.body.credential,
+      audience: config.google.clientId,
+    });
+
+    const payload = ticket.getPayload();
+
+    if (!payload) {
+      return res.status(400).send({ error: "Google login error!" });
+    }
+
+    const email = payload["email"];
+    const googleId = payload["sub"];
+    const username = payload["given_name"];
+    const displayName = payload["name"];
+
+    if (!email && !username) {
+      return res
+        .status(400)
+        .send({ error: "Not enough user data to continue!" });
+    }
+
+    let user = await User.findOne({ googleId });
+
+    if (!user) {
+      user = new User({
+        username,
+        password: randomUUID(),
+        googleId,
+        displayName,
+      });
+      return;
+    }
+
+    user.generateToken();
+    await user.save();
+    return res.send({
+      message: "Login with Goggle us success!",
       user,
     });
   } catch (e) {
